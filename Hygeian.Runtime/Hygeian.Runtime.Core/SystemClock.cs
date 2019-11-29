710363189af84b8e35f0f1d8f891756637c5e0a2
one-sword <aspjet@msn.com>
2019-11-29 10:56:39 +0800

    初始化

---
+++ Hygeian.Runtime/Hygeian.Runtime.Core/SystemClock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Hygeian.Runtime.Core
+{
+    public static class SystemClock
+    {
+        private static DateTimeOffset TimestampStartTime { get; set; } = new DateTimeOffset(1970, 01, 01, 0, 0, 0, TimeSpan.Zero);
+
+        private static TimeSpan DefaultTimeSpan { get; } = TimeSpan.FromHours(8);
+
+        public static DateTimeOffset Now
+        {
+            get { return DateTimeOffset.UtcNow.ToOffset(DefaultTimeSpan); }
+        }
+
+        public static DateTimeOffset FromTimestamp(int timestamp, TimeSpan? offSet = null)
+        {
+            if (offSet.HasValue)
+                return TimestampStartTime.AddSeconds(timestamp).ToOffset(offSet.Value);
+            else
+                return TimestampStartTime.AddSeconds(timestamp).ToOffset(DefaultTimeSpan);
+        }
+
+        public static DateTimeOffset ToDateTimeOffSet(DateTime dateTime)
+        {
+            return new DateTimeOffset(dateTime, DefaultTimeSpan);
+        }
+
+        public static DateTimeOffset ToDateTimeOffSet(string dateTime)
+        {
+            DateTime dt = DateTime.Parse(dateTime);
+            return ToDateTimeOffSet(dt);
+        }
+    }
+}
